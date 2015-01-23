6d307054bbf24cc6a01a70a9243c60dbff7adecf
Adam <guernica0131@gmail.com>
2015-01-23 12:54:13 +0500

    another update to connection.js

--- config/connections.js
+++ config/connections.js
@@ -62,14 +62,14 @@ module.exports.connections = {
     port: 27017,  
   },
 
-  someMongodbServer: {
-    adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
-  },
+  // someMongodbServer: {
+  //   adapter: 'sails-mongo',
+  //   host: 'localhost',
+  //   port: 27017,
+  //   // user: 'username',
+  //   // password: 'password',
+  //   // database: 'your_mongo_db_name_here'
+  // },
 
   
 
