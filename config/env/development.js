444a4f46f37a1651b016aaf585509ac1983df67d
Adam <guernica0131@gmail.com>
2015-01-23 14:29:30 +0500

    for local access

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
    models: {
-    connection: 'dockerMongo'
+    connection: 'localDiskDb'
    }
 
 };
