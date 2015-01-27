5819a2b3a516a13de86a26fef2bb22738b6f8657
Adam <guernica0131@gmail.com>
2015-01-27 09:35:44 +0500

    Set to docker

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
    models: { 
-    connection: 'localDiskDb' //localDiskDb //dockerPostgress
+    connection: 'dockerPostgress' //localDiskDb //dockerPostgress
    }
 
 };
